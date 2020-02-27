fb9eac7f7c325ff924a81f163665648f8f88cc4b
Fábio Santos <delith@gmail.com>
2020-02-27 15:26:30 -0300

    Primeiro envio de arquivos

---
+++ index.js
@@ -0,0 +1,12 @@
+// Nesse arquivos eu estou apenas escrevendo varios comandos junto ao instrutor
+// para treino
+
+const alunos = [
+    {nome:'Fabio',nota:9.8},
+    {nome:'Jose',nota:5},
+    {nome:'Pedro',nota:8}
+]
+
+const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota)/3
+
+console.log (media)
